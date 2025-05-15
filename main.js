77c4e856115cfdb764aac26805bcc4c0c6bfcafa
leventkarabiber <leventkarabiber@hotmail.com>
2025-05-15 23:31:34 +0300

    init

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("code uploaded");
+let button = document.getElementById("YRAQ");
+button.addEventListener("click", function () {
+  console.log("tanersins");
+  window.alert("joker benim yasins");
+});
